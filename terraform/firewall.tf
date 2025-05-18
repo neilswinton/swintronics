c84475ab4feb5c9e9595aa0cde0db886ef1e5ec4
Neil Swinton <5995552+neilswinton@users.noreply.github.com>
2025-05-18 12:06:19 -0400

    Restrict ssh and http to deploy system IP address

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -1,19 +1,27 @@
+data "http" "myip" {
+  url = "https://ipv4.icanhazip.com"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "text/plain"
+  }
+}
+
+
 resource "hcloud_firewall" "cluster" {
   name = var.name
 
   rule {
-    direction = "in"
-    protocol  = "tcp"
-    port      = "22"
-    source_ips = ["0.0.0.0/0",
-    "::/0"]
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "22"
+    source_ips = ["${chomp(data.http.myip.response_body)}/32"]
   }
   rule {
-    direction = "in"
-    protocol  = "tcp"
-    port      = "80"
-    source_ips = ["0.0.0.0/0",
-    "::/0"]
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "80"
+    source_ips = ["${chomp(data.http.myip.response_body)}/32"]
   }
   rule {
     direction = "in"
