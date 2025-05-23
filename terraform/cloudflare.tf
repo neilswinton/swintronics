c7959abc3b6277a317d9e58e2a22a12c64c43bfa
Neil Swinton <5995552+neilswinton@users.noreply.github.com>
2025-05-23 11:21:11 -0400

    Add unproxied DNS entry for ssh access to server

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -12,3 +12,13 @@ resource "cloudflare_dns_record" "www" {
   type    = "A"
   zone_id = var.cloudflare_zone_id
 }
+
+resource "cloudflare_dns_record" "ssh" {
+  name = "ssh"
+
+  content = hcloud_server.server.ipv4_address
+  proxied = false
+  ttl     = 1
+  type    = "A"
+  zone_id = var.cloudflare_zone_id
+}
