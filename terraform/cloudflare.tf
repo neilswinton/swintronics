74219e152b49467481c9a017fde8c3f751fe2220
Neil Swinton <5995552+neilswinton@users.noreply.github.com>
2025-06-08 22:21:44 -0400

    Set DNS root and ssh to cluster IP.  Proxied CNAME for servers

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -8,15 +8,26 @@ resource "cloudflare_dns_record" "service_dns" {
   for_each = toset(var.services)
   name     = "${each.key}.${var.domain}"
 
-  content = hcloud_server.server[0].ipv4_address
+  content = var.domain
   proxied = true
+  ttl     = 1
+  type    = "CNAME"
+  zone_id = var.cloudflare_zone_id
+
+}
+
+resource "cloudflare_dns_record" "ssh" {
+  name = "ssh.${var.domain}"
+
+  content = hcloud_server.server[0].ipv4_address
+  proxied = false
   ttl     = 1
   type    = "A"
   zone_id = var.cloudflare_zone_id
 }
 
-resource "cloudflare_dns_record" "ssh" {
-  name = "ssh.${var.domain}"
+resource "cloudflare_dns_record" "root" {
+  name = "@"
 
   content = hcloud_server.server[0].ipv4_address
   proxied = false
