067e63305f0db30bccba0626181e7b41972fb0ef
Neil Swinton <5995552+neilswinton@users.noreply.github.com>
2025-09-17 20:47:04 -0400

    Use wildcard for webservices in DNS

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -16,19 +16,6 @@ data "cloudflare_ip_ranges" "whitelist" {
 
 }
 
-resource "cloudflare_dns_record" "service_dns" {
-
-  for_each = toset(var.services)
-  name     = "${each.key}.${var.domain}"
-
-  content = var.domain
-  proxied = true
-  ttl     = 1
-  type    = "CNAME"
-  zone_id = var.cloudflare_zone_id
-
-}
-
 resource "cloudflare_dns_record" "ssh" {
   name = "ssh.${var.domain}"
 
@@ -41,6 +28,16 @@ resource "cloudflare_dns_record" "ssh" {
 
 resource "cloudflare_dns_record" "root" {
   name = "@"
+
+  content = hcloud_server.server[0].ipv4_address
+  proxied = false
+  ttl     = 1
+  type    = "A"
+  zone_id = var.cloudflare_zone_id
+}
+
+resource "cloudflare_dns_record" "webservices" {
+  name = "*.ts"
 
   content = hcloud_server.server[0].ipv4_address
   proxied = false
