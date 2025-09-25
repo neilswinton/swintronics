3c03196a61bee633b6943421f689b583612c596e
Neil Swinton <5995552+neilswinton@users.noreply.github.com>
2025-09-25 14:25:35 -0400

    Get webservice IP from live tailscale devices

--- terraform/tailscale.tf
+++ terraform/tailscale.tf
@@ -1,10 +1,10 @@
 # Create an identity for the tailscale docker container and save it to the secrets vault
 
 locals {
-  tailscale_container_tag = "container"
+  tailscale_container_tag = "tag:container"
 }
 resource "tailscale_oauth_client" "docker_identity" {
   description = "Docker OAuth client for ${var.project_name}"
   scopes      = ["devices:core", "auth_keys"]
-  tags        = ["tag:${local.tailscale_container_tag}"]
+  tags        = [local.tailscale_container_tag]
 }
