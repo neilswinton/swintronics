16a1e4c282027bc4f2b5e3ef813479ce8edcf112
Neil Swinton <5995552+neilswinton@users.noreply.github.com>
2026-01-30 20:14:55 -0500

    Move tailscale container oauth credentials

--- terraform/tailscale.tf
+++ terraform/tailscale.tf
@@ -1,14 +1,7 @@
 # Create an identity for the tailscale docker container and save it to the secrets vault
 
 locals {
-  tailscale_container_tag = "tag:container"
-  tailscale_server_tag    = "tag:server"
-}
-
-resource "tailscale_oauth_client" "docker_identity" {
-  description = "Docker OAuth client for ${var.project_name}"
-  scopes      = ["devices:core", "auth_keys"]
-  tags        = [local.tailscale_container_tag]
+  tailscale_server_tag = "tag:server"
 }
 
 resource "tailscale_tailnet_key" "swintronics_auth" {
